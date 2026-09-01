deada871551ad363be53d6412281713909b02aa3
412-15-thitiphan <54404@yupparaj.ac.th>
2026-09-01 10:59:18 +0700

    Update vocad_game.py

--- vocad_game.py
+++ vocad_game.py
@@ -27,7 +27,7 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
@@ -51,13 +51,13 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-    if u_ans3 == "Watermelon":
+    if u_ans3 == "watermelon":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 # ตรวจข้อ 4
-    if u_ans2 == "key":
+    if u_ans4 == "key":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
