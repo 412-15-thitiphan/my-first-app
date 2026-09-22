10a20d1c3940127e49f46b6fff02378b60f1d479
412-15-thitiphan <54404@yupparaj.ac.th>
2026-09-22 10:30:24 +0700

    Update ท้ายคำศัพท์ภาษาอังกฤษB1,B2app.py

--- ท้ายคำศัพท์ภาษาอังกฤษB1,B2app.py
+++ ท้ายคำศัพท์ภาษาอังกฤษB1,B2app.py
@@ -78,9 +78,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5):
 
     if score == 5:
         st.success("🎉 You win!")
-    if score == 2-4:
-        st.success("👍 You good!")
-    if score == 0-1:
+    else:
         st.error("💀 You lose!")
 
 
